649572df23bf1ac3ef3005863d074f7bb7618808
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2024-01-22 19:55:51 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,16 +12,19 @@ import { TimerModule } from './timer/timer.module';
 import { Users } from './user/entities/user.entity';
 import { Timer } from './timer/entities/timer.entity';
 
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'pomoappdb',
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Users, Timer],
       synchronize: true,
       },
