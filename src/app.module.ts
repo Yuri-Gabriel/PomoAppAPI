815ef47f1d1c80c4eff594579e4829470aa9c65c
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2024-01-24 08:03:11 -0300

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: "mysql",
+      type: process.env.DB_TYPE as any,
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
